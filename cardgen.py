d518fc00340803b1bb11daad5fa1cd2b82b39e78
alexbowe <bowe.alexander@gmail.com>
2010-06-08 00:30:12 +1000

    add python script for reading flashcard definitions and filling in the template

---
+++ cardgen.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from Cheetah.Template import Template
+import re, sys, optparse
+
+template = 'template.tex'
+re_text = '^(.+):$\n((?:\n?.+)+)\n*'
+rec = re.compile(re_text, re.MULTILINE)
+
+# Eat stderr
+class Discarder(object):
+    def write(self, text):
+        pass
+
+def getMatches(file):
+    text = file.read()
+    matches = rec.finditer(text)
+    return matches
+
+def main():
+    p = optparse.OptionParser()
+    p.add_option('-i', '--input', help='Card definition file')
+    p.add_option('-o', '--output', help='Output File')
+    options, args = p.parse_args()
+    
+    infile = sys.stdin
+    outfile = sys.stdout
+    
+    if options.input is not None:
+        infile = open(str(options.input), 'r')
+        
+    if options.output is not None:
+        outfile = open(str(options.output), 'w')
+    
+    cards = getMatches(infile)
+    
+    # get rid of cheetah stderr output
+    temp = sys.stderr
+    sys.stderr = Discarder()
+    outfile.write(str(Template(file=template, searchList = { 'cards': cards })))
+    # restore stderr
+    sys.stderr = temp
+    
+    if options.input is not None:
+        infile.close()
+    if options.output is not None:
+        outfile.close()
+    
+if __name__ == '__main__':
+    main()
