c519374c86678c83c9deb5253a09046fd4d30f14
alexbowe <bowe.alexander@gmail.com>
2011-10-24 00:48:30 +1100

    hacked up formatting, not nested

--- cardgen.py
+++ cardgen.py
@@ -9,6 +9,25 @@ template = 'template.tex'
 re_text = r'^(.+):$\n((?:\n?.+)+)\n*'
 rec = re.compile(re_text, re.MULTILINE)
 
+# initial pass maps delimiters to formatting
+formats = { r'\*' : r'\textbf{%s}',
+            r'\+': r'\emph{%s}',
+            '_': r'\underline{%s}' }
+
+def text_mapper(formats):
+    regex = r''
+    for (i, (delimiter, _)) in enumerate(formats.items()):
+        if i > 0: regex += '|'
+        regex += r'%s(.*)%s' % (delimiter, delimiter)
+    r = re.compile(regex)
+    def map_match(match):
+        for (i, item) in enumerate(match.groups()):
+            if item is not None:
+                fmt = formats.items()[i][1]
+                return fmt % item
+    sub = lambda string: r.sub(map_match, string)
+    return sub
+
 # Eat stderr
 class Discarder(object):
     def write(self, text):
@@ -32,7 +51,8 @@ def compileFromString(instring):
 
 def compile(infile, outfile):
     instring = infile.read()
-    output = compileFromString(instring)
+    transform = text_mapper(formats)
+    output = compileFromString(transform(instring))
     outfile.write(output)
     
 def fileHandledExec(func, infile=None, outfile=None):
